a79ead6e6bcc50c2d607fc5d5333320d60360571
Evgeny Bednyakov <30568068+Visiterius@users.noreply.github.com>
2023-01-24 13:55:43 +0200

    first com

---
+++ app/news/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import {Button} from "@mui/material";
+
+export default async function Page(){
+    return (
+        <div>
+            <Button variant='contained'>TESyy</Button>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+        </div>
+    )
+}
+
+
